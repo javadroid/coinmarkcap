97dd544115407d1612cb442c3bfbc241a22680e6
Javadroid <ochigboemmanuel32@gmail.com>
2024-05-31 01:16:43 +0100

    Added header to components

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    reactStrictMode: true,
+    images: {
+      domains: ['encrypted-tbn0.gstatic.com', 'cdn.sanity.io','s2.coinmarketcap.com', 's3.coinmarketcap.com', 'cdn.sanity.io', 'encrypted-tbn0.gstatic.com'],
+    },
+};
 
 export default nextConfig;
